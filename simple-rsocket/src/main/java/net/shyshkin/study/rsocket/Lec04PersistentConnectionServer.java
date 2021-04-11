3253bd793a77ff1685dd5e41511ba46b92ee679e
Art <d.art.shishkin@gmail.com>
2021-04-11 17:38:34 +0300

    27. Persistent Connection Issue Fix (#1)

--- simple-rsocket/src/main/java/net/shyshkin/study/rsocket/Lec04PersistentConnectionServer.java
+++ simple-rsocket/src/main/java/net/shyshkin/study/rsocket/Lec04PersistentConnectionServer.java
@@ -3,15 +3,17 @@ package net.shyshkin.study.rsocket;
 import io.rsocket.core.RSocketServer;
 import io.rsocket.transport.netty.server.CloseableChannel;
 import io.rsocket.transport.netty.server.TcpServerTransport;
+import lombok.extern.slf4j.Slf4j;
 import net.shyshkin.study.rsocket.service.FastProducerService;
 import reactor.core.publisher.Mono;
 
+@Slf4j
 public class Lec04PersistentConnectionServer {
 
     public static void main(String[] args) {
         RSocketServer rSocketServer = RSocketServer.create((setup, sendingSocket) -> Mono.just(new FastProducerService()));
         CloseableChannel closeableChannel = rSocketServer.bindNow(TcpServerTransport.create(6565));
-
+        log.debug("Server started successfully: {}", rSocketServer);
         //keep listening
         closeableChannel.onClose().block();
     }
--- simple-rsocket/src/test/java/net/shyshkin/study/rsocket/Lec04PersistentConnectionManualTest.java
+++ simple-rsocket/src/test/java/net/shyshkin/study/rsocket/Lec04PersistentConnectionManualTest.java
@@ -2,6 +2,7 @@ package net.shyshkin.study.rsocket;
 
 import io.rsocket.Payload;
 import io.rsocket.RSocket;
+import io.rsocket.core.RSocketClient;
 import io.rsocket.core.RSocketConnector;
 import io.rsocket.transport.netty.client.TcpClientTransport;
 import io.rsocket.util.DefaultPayload;
@@ -10,22 +11,25 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 import java.time.Duration;
 
 @Slf4j
-@DisplayName("Start server `Lec04PersistentConnectionServer`, run test, during 15s pause restart server - will catch java.nio.channels.ClosedChannelException")
+@DisplayName("Start server `Lec04PersistentConnectionServer`, run test, during 15s pause restart server - now all ok, NO exception java.nio.channels.ClosedChannelException")
 class Lec04PersistentConnectionManualTest {
 
-    private static RSocket rSocket;
+    private static RSocketClient rSocketClient;
 
     @BeforeAll
     static void beforeAll() {
 
-        rSocket = RSocketConnector.create()
+        Mono<RSocket> rSocketMono = RSocketConnector.create()
                 .connect(TcpClientTransport.create(6565))
-                .block();
+                .doOnNext(rs -> log.debug("going to connect"));
+
+        rSocketClient = RSocketClient.from(rSocketMono);
     }
 
     @Test
@@ -34,7 +38,7 @@ class Lec04PersistentConnectionManualTest {
         Payload payload = DefaultPayload.create("NO Matter");
 
         //when
-        Flux<String> flux1 = rSocket.requestStream(payload)
+        Flux<String> flux1 = rSocketClient.requestStream(Mono.just(payload))
                 .delayElements(Duration.ofMillis(50)) //slow client process emulation
                 .map(Payload::getDataUtf8)
                 .doOnNext(dto -> log.debug("client: receive {}", dto))
@@ -49,7 +53,7 @@ class Lec04PersistentConnectionManualTest {
         Thread.sleep(15000);
         log.debug("woke up");
 
-        Flux<String> flux2 = rSocket.requestStream(payload)
+        Flux<String> flux2 = rSocketClient.requestStream(Mono.just(payload))
                 .delayElements(Duration.ofMillis(50)) //slow client process emulation
                 .map(Payload::getDataUtf8)
                 .doOnNext(dto -> log.debug("client: receive {}", dto))

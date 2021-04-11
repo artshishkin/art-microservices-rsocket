183f485340bf8cd5a17eca2aa3d0d19625584890
Art <d.art.shishkin@gmail.com>
2021-04-11 20:58:53 +0300

    31. Introduction (#2 Section 4: RSocket With Spring)

---
+++ spring-rsocket/src/test/java/net/shyshkin/study/rsocket/springrsocket/SpringRsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package net.shyshkin.study.rsocket.springrsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRsocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

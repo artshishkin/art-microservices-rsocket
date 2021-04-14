1e379bfb1c6f8e8f6a0dd305b9772d18adeb1ed9
Art <d.art.shishkin@gmail.com>
2021-04-14 09:20:51 +0300

    84. Reactive User Details Service (#6)

---
+++ spring-rsocket/src/main/java/net/shyshkin/study/rsocket/springrsocket/security/UserDetailsServiceImpl.java
@@ -0,0 +1,19 @@
+package net.shyshkin.study.rsocket.springrsocket.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements ReactiveUserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public Mono<UserDetails> findByUsername(String username) {
+        return Mono.fromSupplier(() -> userRepository.findByUsername(username));
+    }
+}

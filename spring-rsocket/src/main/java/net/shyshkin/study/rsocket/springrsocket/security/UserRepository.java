7f62e449fe81fc0a51c467d46401fbbe440bd75a
Art <d.art.shishkin@gmail.com>
2021-04-14 09:17:33 +0300

    83. Fake User Repository (#6)

---
+++ spring-rsocket/src/main/java/net/shyshkin/study/rsocket/springrsocket/security/UserRepository.java
@@ -0,0 +1,35 @@
+package net.shyshkin.study.rsocket.springrsocket.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Repository;
+
+import javax.annotation.PostConstruct;
+import java.util.Map;
+
+@Repository
+@RequiredArgsConstructor
+public class UserRepository {
+
+    private final PasswordEncoder encoder;
+
+    private Map<String, UserDetails> db;
+
+    @PostConstruct
+    private void init() {
+
+        db = Map.of(
+                "user01", User.withUsername("user01").password(encoder.encode("pass01u")).roles("USER").build(),
+                "user02", User.withUsername("user02").password(encoder.encode("pass02u")).roles("USER").build(),
+                "admin01", User.withUsername("admin01").password(encoder.encode("pass03a")).roles("ADMIN").build(),
+                "client01", User.withUsername("client01").password(encoder.encode("pass04c")).roles("TRUSTED_CLIENT").build()
+        );
+    }
+
+    public UserDetails findByUsername(String username) {
+        return db.get(username);
+    }
+
+}
